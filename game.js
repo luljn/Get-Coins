c0bcdf258065bb9b6558db9c0e3f4d7132997057
luljn <mbohlulajonathan4@gmail.com>
2025-09-14 10:30:21 +0200

    v1.1 hit enemy

--- game.js
+++ game.js
@@ -3,12 +3,14 @@ class mainScene {
 
     // Load assets.
     preload() {
-
+        
         this.load.image('player', 'assets/ghost.png');
         this.load.image('coin', 'assets/coin.png');
         this.load.image('enemy', 'assets/enemy.png');
         this.width = 1200;
         this.height = 500;
+        this.score = 0;     // To store the score.
+        this.score -= 1;
     }
 
     // Initialize scene.
@@ -17,22 +19,21 @@ class mainScene {
         this.player = this.physics.add.sprite(60, 60, 'player');
         this.coin = this.physics.add.sprite(0, 0, 'coin');
         this.enemy1 = this.physics.add.sprite(Phaser.Math.Between(10, this.width-10), Phaser.Math.Between(10, this.height-10), 'enemy');
-        this.enemy2 = this.physics.add.sprite(Phaser.Math.Between(10, this.width-10), Phaser.Math.Between(10, this.height-10), 'enemy');
+        /* this.enemy2 = this.physics.add.sprite(Phaser.Math.Between(10, this.width-10), Phaser.Math.Between(10, this.height-10), 'enemy');
         this.enemy3 = this.physics.add.sprite(Phaser.Math.Between(10, this.width-10), Phaser.Math.Between(10, this.height-10), 'enemy');
-        this.enemy4 = this.physics.add.sprite(Phaser.Math.Between(10, this.width-10), Phaser.Math.Between(10, this.height-10), 'enemy');
-        this.enemies = [this.enemy1, this.enemy2, this.enemy3, this.enemy4];
+        this.enemy4 = this.physics.add.sprite(Phaser.Math.Between(10, this.width-10), Phaser.Math.Between(10, this.height-10), 'enemy'); */
+        this.sprites = [this.player, this.coin, this.enemy1/* , this.enemy2, this.enemy3, this.enemy4 */];
         this.player.setScale(0.05);
         this.coin.setScale(0.045);
         this.enemy1.setScale(0.07);
-        this.enemy2.setScale(0.07);
+        /* this.enemy2.setScale(0.07);
         this.enemy3.setScale(0.07);
-        this.enemy4.setScale(0.07);
+        this.enemy4.setScale(0.07); */
 
-        this.score = 0 // To store the score.
-        let style = {font: '20px Arial', fill: '#fff'}
-        this.scoreText = this.add.text(20, 20, 'score : ' + this.score, style)
+        let style = {font: '20px Arial', fill: '#fff'};
+        this.scoreText = this.add.text(20, 20, 'score : ' + this.score, style);
 
-        this.arrow = this.input.keyboard.createCursorKeys()
+        this.arrow = this.input.keyboard.createCursorKeys();
     }
 
     // Handle the game logic.
@@ -44,8 +45,8 @@ class mainScene {
         }
 
         // If the player and an enemy overlapping.
-        if (this.physics.overlap(this.player, this.enemy1) || (this.physics.overlap(this.player, this.enemy2)) || 
-            this.physics.overlap(this.player, this.enemy3) || (this.physics.overlap(this.player, this.enemy4))) {
+        if (this.physics.overlap(this.player, this.enemy1) /* || (this.physics.overlap(this.player, this.enemy2)) || 
+            this.physics.overlap(this.player, this.enemy3) || (this.physics.overlap(this.player, this.enemy4)) */) {
                 this.hitEnemy();
         }
 
@@ -70,14 +71,14 @@ class mainScene {
 
     // Handle collisions with the coin.
     hit() {
-
-        this.coin.x = Phaser.Math.Between(10, this.width-10);
-        this.coin.y = Phaser.Math.Between(10, this.height-10);
+        
         this.score += 1;
         this.scoreText.setText('score : ' + this.score);
+        this.coin.x = Phaser.Math.Between(10, this.width-10);
+        this.coin.y = Phaser.Math.Between(10, this.height-10);
         this.tweens.add({
             targets: this.player,
-            duration: 200,
+            duration: 100,
             scaleX: 0.09,
             scaleY: 0.09,
             yoyo: true,
@@ -89,6 +90,7 @@ class mainScene {
 
         let style = {font: '50px Arial', fill: '#FF0000'};
         this.gameOverText = this.add.text(50, 50, 'Game Over !', style);
+        window.location.reload();
     }
 
     // Manage the movement of enemies.
